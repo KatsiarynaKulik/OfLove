37e0673c2461a8839587ee12929c3ff446b4d4ec
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-05-29 23:45:16 +0400

    Initial Commit

---
+++ OfLove/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OfLove
+//
+//  Created by Екатерина  on 29.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
