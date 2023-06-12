21caaf74e50ec383a3589e3b4002e20f8140b65c
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-06-13 00:21:32 +0400

    Add: animation setting

--- OfLove/TimerViewController.swift
+++ OfLove/TimerViewController.swift
@@ -11,12 +11,73 @@ import UIKit
 class TimerViewController: UIViewController {
 
   @IBOutlet var labelTimer: UILabel!
+  @IBOutlet var timerImageView: UIImageView!
+  @IBOutlet var timetLabel: UILabel!
   @IBOutlet var startButton: UIButton!
+  @IBOutlet var stopButton: UIButton!
+
+  var timer = Timer()
+
+  let shapeLayer = CAShapeLayer()
+
+  var durationTimer = 240
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    self.animationCircular()
+  }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+      startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
     }
 
+  @objc func startButtonTapped() {
+
+    basicAnimation()
+
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timetAction), userInfo: nil, repeats: true)
+  }
+
+  @objc func timetAction() {
+
+    durationTimer -= 1
+    timetLabel.text = "\(durationTimer)"
+    print("\(durationTimer)")
+
+    if durationTimer == 0 {
+      timer.invalidate()
+    }
+  }
+
+  //MARK: Animation
+
+  func animationCircular() {
+
+    let center = CGPoint(x: timerImageView.frame.width / 2, y: timerImageView.frame.height / 2)
+
+    let endAngle = (-CGFloat.pi / 2)
+    let startAngle = 2 * CGFloat.pi + endAngle
+
+    let circularPath = UIBezierPath(arcCenter: center, radius: 138, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+
+    shapeLayer.path = circularPath.cgPath
+    shapeLayer.lineWidth = 9
+    shapeLayer.fillColor = UIColor.clear.cgColor
+    shapeLayer.strokeEnd = 1
+    shapeLayer.lineCap = CAShapeLayerLineCap.round
+    shapeLayer.strokeColor = #colorLiteral(red: 0.8462027907, green: 0.07871665806, blue: 0.2146175206, alpha: 1).cgColor
+    timerImageView.layer.addSublayer(shapeLayer)
+  }
+
+  func basicAnimation() {
+    let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+
+    basicAnimation.toValue = 0
+    basicAnimation.duration = CFTimeInterval(durationTimer)
+    basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+    basicAnimation.isRemovedOnCompletion = true
+    shapeLayer.add(basicAnimation, forKey: "basicAnimation")
+  }
 }
