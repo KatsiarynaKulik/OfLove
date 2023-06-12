70f813fb580eecde48266144d9d763661f703214
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-06-12 23:02:55 +0400

    Add: timer setting

---
+++ OfLove/TimerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TimerViewController.swift
+//  OfLove
+//
+//  Created by Katsiaryna Kulik  on 12.06.23.
+//  Copyright © 2023 Katsiaryna Kulik. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+  @IBOutlet var labelTimer: UILabel!
+  @IBOutlet var startButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
